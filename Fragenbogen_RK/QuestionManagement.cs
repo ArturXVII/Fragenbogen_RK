8b2dc558076d6738d827d189a05555db44d64438
Max <max_otti@web.de>
2019-03-06 16:27:54 +0100

    Added DBContexts + QuestionManagement

---
+++ Fragenbogen_RK/QuestionManagement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fragenbogen_RK
+{
+    class QuestionManagement
+    {
+        Fragen fragenEntitie = new Fragen();
+        Antworten antwortenEntitie = new Antworten();
+    }
+}
