f85e87b73b0bb5ee75dfdf16f36652aacb20c71e
Jamie K <tetsuyagl@gmx.de>
2019-03-06 15:34:37 +0100

    basic gui

---
+++ Fragenbogen_RK/GUIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fragenbogen_RK
+{
+    class GUIController
+    {
+        public string GetQuestion(int i)
+        {
+            return "Ist zweite Hilfe auch noch ok?";
+        }
+
+        public string[] GetAnswers(int i)
+        {
+            string[] items = { "Item1", "Item2", "Item3", "Item4" };
+            return items;
+        }
+
+        public Boolean IsCorrect(int i, string[] answers)
+        {
+            string[] correctAnswers = {"Item1", "Item2"};
+            if (correctAnswers.Length != answers.Length) return false;
+            foreach (string answer in answers)
+            {
+                if (Array.IndexOf(correctAnswers, answer) <= -1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- Fragenbogen_RK/MainWindow.xaml.cs
+++ Fragenbogen_RK/MainWindow.xaml.cs
@@ -20,9 +20,53 @@ namespace Fragenbogen_RK
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GUIController controller;
+        private int score = 0;
+        private int questionIndex = 1;
+        private string[] checkedAnswers;
         public MainWindow()
         {
             InitializeComponent();
+            controller = new GUIController();
+            SetQuiz(0);
+        }
+
+        private void SetQuiz(int i)
+        {
+            lblFrage.Content = "Frage " + questionIndex + ": " + controller.GetQuestion(i);
+            SetQuestions(controller.GetAnswers(i));
+        }
+
+        private void SetQuestions(string[] questions)
+        {
+            for (int i = 0; i < questions.Length; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        chkA.Content = questions[i];
+                        break;
+                    case 1:
+                        chkB.Content = questions[i];
+                        break;
+                    case 2:
+                        chkC.Content = questions[i];
+                        break;
+                    case 3:
+                        chkD.Content = questions[i];
+                        break;
+                }
+            }
+        }
+
+        private void chkA_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnWeiter_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
