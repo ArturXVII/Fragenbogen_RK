e60c1c097ea3cf96d10eed0a03301f6226aaa914
Artur Jekimow <arturio@hotmail.de>
2019-03-28 21:35:27 +0100

    Test Ergänzungen und Unnötige Tests Löschung

--- Fragenbogen_RK/QuestionTests.cs
+++ Fragenbogen_RK/QuestionTests.cs
@@ -13,16 +13,29 @@ namespace Fragenbogen_RK
     {
 
         [TestMethod]
-        public void QuestionManagement_getQuestionByIdTests()
+        public void QuestionManagement_getQuestionByIdTest_ReturnValue()
         {
             QuestionManagement qManager = new QuestionManagement();
             Question q = qManager.getQuestionById(1);
 
             Assert.AreEqual(q.answers[0].GetAnswer(), "122");
+            Assert.AreEqual(q.answers[1].GetAnswer(), "133");
+            Assert.AreEqual(q.answers[2].GetAnswer(), "144");
+            Assert.AreEqual(q.answers[3].GetAnswer(), "112");
         }
 
         [TestMethod]
-        public void QuestionManagement_QuestionSet()
+        [ExpectedException(typeof(NullReferenceException))]
+        public void QuestionManagement_getQuestionById0Test_Return_NullReferenceException()
+        {
+            QuestionManagement qManager = new QuestionManagement();
+            Question q = qManager.getQuestionById(0);
+
+            Assert.IsNotNull(q.question);
+        }
+
+        [TestMethod]
+        public void QuestionManagement_QuestionSet_ReturnValues()
         {
             QuestionManagement qManager = new QuestionManagement();
 
